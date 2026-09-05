6a9c2eff10ab422592a78e4bb99612b8021b2a99
BhalchandraNalawade <bhalchandranalawade12@gmail.com>
2026-09-05 07:48:17 +0530

    code is changed

--- src/main/java/com/insurance/InsurancePolicyManagementApplication.java
+++ src/main/java/com/insurance/InsurancePolicyManagementApplication.java
@@ -9,6 +9,7 @@ public class InsurancePolicyManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InsurancePolicyManagementApplication.class, args);
                 int i = 10;
+                int j = 20;
 	}
 
 }
