03d9db48fe37bce0be1a934340dff2ec483daa75
BhalchandraNalawade <bhalchandranalawade12@gmail.com>
2026-09-05 16:24:35 +0530

    Revert "Int var added"
    This reverts commit 45f15e5add4f66c19e283688c87a0fcd9188faf4.

--- src/main/java/com/insurance/InsurancePolicyManagementApplication.java
+++ src/main/java/com/insurance/InsurancePolicyManagementApplication.java
@@ -14,8 +14,6 @@ public class InsurancePolicyManagementApplication {
 
        public static void main(String[] args) {
 		SpringApplication.run(InsurancePolicyManagementApplication.class, args);
-
-                int i = 2012;
                
 	}
 
